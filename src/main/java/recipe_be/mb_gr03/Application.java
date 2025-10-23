82962dceb1e2124f4ae2b85c576b94b3dcae09ec
QuangTrongLeo <trongphamtg05@gmail.com>
2025-10-23 13:15:32 +0700

    Initial commit

---
+++ src/main/java/recipe_be/mb_gr03/Application.java
@@ -0,0 +1,13 @@
+package recipe_be.mb_gr03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/recipe_be/mb_gr03/ApplicationTests.java
@@ -0,0 +1,13 @@
+package recipe_be.mb_gr03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
