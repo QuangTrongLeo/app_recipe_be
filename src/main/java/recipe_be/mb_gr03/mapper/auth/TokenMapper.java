94ed595ae8f1a1198982d1749bf01db08cb5d1bf
QuangTrongLeo <trongphamtg05@gmail.com>
2025-10-31 11:49:37 +0700

    fix: update TokenResponse

--- src/main/java/recipe_be/mb_gr03/mapper/auth/TokenMapper.java
+++ src/main/java/recipe_be/mb_gr03/mapper/auth/TokenMapper.java
@@ -6,7 +6,7 @@ import recipe_be.mb_gr03.dto.response.auth.TokenResponse;
 @Component
 public class TokenMapper {
 
-    public TokenResponse toTokenResponse(String accessToken, String refreshToken) {
-        return new TokenResponse(accessToken, refreshToken);
+    public TokenResponse toTokenResponse(String id, String accessToken, String refreshToken) {
+        return new TokenResponse(id, accessToken, refreshToken);
     }
 }
