bc44cc7b15a1f9c1615159d50ecceb428c0bb6ea
QuangTrongLeo <trongphamtg05@gmail.com>
2025-11-01 12:33:49 +0700

    fix: user annotation mapping for interface mapping

--- src/main/java/recipe_be/mapper/CategoryMapper.java
+++ src/main/java/recipe_be/mapper/CategoryMapper.java
@@ -1,6 +1,7 @@
 package recipe_be.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import recipe_be.dto.response.CategoryResponse;
 import recipe_be.entity.Category;
 
@@ -8,6 +9,8 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "status", source = "status")
     CategoryResponse toCategoryResponse(Category category);
     List<CategoryResponse> toCategoryResponseList(List<Category> categories);
 }
--- src/main/java/recipe_be/mapper/UserMapper.java
+++ src/main/java/recipe_be/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package recipe_be.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import recipe_be.dto.response.UserResponse;
 import recipe_be.entity.User;
 import recipe_be.enums.Role;
@@ -9,9 +10,8 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target = "role", expression = "java(user.getRole() != null ? user.getRole().name() : null)")
     UserResponse toUserResponse(User user);
+
     List<UserResponse> toUserResponseList(List<User> users);
-    default String map(Role role) {
-        return role == null ? null : role.name();
-    }
 }
